9af247687a73508b7e422cf15c96168bdb447b3c
rodrigo araujo santos <araujo.1983@hotmail.com>
2022-07-25 10:12:43 -0300

    validates and atributions

---
+++ app/models/list.rb
@@ -0,0 +1,7 @@
+class List < ApplicationRecord
+  has_many :bookmarks
+  has_many :movies, dependent: :destroy
+  has_many :movies, through: :bookmarks
+  validates :name, presence: true, uniqueness: true
+ 
+end

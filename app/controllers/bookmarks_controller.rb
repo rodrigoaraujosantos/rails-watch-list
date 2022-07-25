2584a117110bca6c348d3cf0eb0c4c024ea269d9
rodrigo araujo santos <araujo.1983@hotmail.com>
2022-07-25 11:27:47 -0300

    controllers

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,22 @@
 class BookmarksController < ApplicationController
   def new
+    @bookmark = Bookmark.new
   end
 
   def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @bookmark
+    @bookmark.save
+    redirect_to list_path(@list)
+
   end
 
   def destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
   end
 end
